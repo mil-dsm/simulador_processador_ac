5d01e5abbd2c6f4b8f2128fe2b7779d58c2e4142
Trizur <anabeatrizcapistranopereira@gmail.com>
2026-01-09 15:15:28 -0300

    Fix comparison logic in OP_CMP case

--- instrucoes_ULA.c
+++ instrucoes_ULA.c
@@ -203,9 +203,9 @@ int main(int argc, char *argv[]) {
             }
             //comparação entre registradores
             case OP_CMP: {
-                int32_t result = cpu.regs[rm] - cpu.regs[rn];
-                cpu.flags.carry = (cpu.regs[rn] > cpu.regs[rm]);
-                cpu.flags.zero = ((int16_t)result == 0);
+                int16_t result = (int16_t){cpu.regs[rm] - cpu.regs[rn]);
+                cpu.flags.carry = (cpu.regs[rm] < cpu.regs[rn]);
+                cpu.flags.zero = (result == 0);
                 break;
             }
 
